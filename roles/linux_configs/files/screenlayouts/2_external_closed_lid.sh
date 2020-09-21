3b0adddeeb7b0926a6a9924691ebbd350f20cb86
Slawek Kaplonski <skaplons@redhat.com>
2020-09-21 09:16:58 +0200

    Change primary screen on my desktop

--- roles/linux_configs/files/screenlayouts/2_external_closed_lid.sh
+++ roles/linux_configs/files/screenlayouts/2_external_closed_lid.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP3 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output VIRTUAL1 --off 2>/dev/null
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP3 --mode 3840x2160 --pos 3840x0 --rotate normal --output VIRTUAL1 --off 2>/dev/null
 if [ $? -ne 0 ]; then
-    xrandr --output eDP1 --off --output DP1 --off --output DP3 --off --output DP3-1 --off --output DP3-2 --off --output DP3-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP2 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output VIRTUAL1 --off 2>/dev/null
+    xrandr --output eDP1 --off --output DP1 --off --output DP3 --off --output DP3-1 --off --output DP3-2 --off --output DP3-3 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP2 --mode 3840x2160 --pos 3840x0 --rotate normal --output VIRTUAL1 --off 2>/dev/null
 fi
