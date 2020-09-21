256c6778ac6d2a9e1c9d16328bed211ff74e164e
Slawek Kaplonski <skaplons@redhat.com>
2020-09-21 21:19:30 +0200

    Add script to set backlight

---
+++ roles/linux_configs/files/i3wm/setbacklight.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+action=$1
+step=$2
+
+current_level=$(/usr/bin/xbacklight -get | awk -F'.' '{print $1}')
+
+if [ "$action" == "dec" ]; then
+    new_level=$(( current_level - step ))
+    if [ $new_level -le 10 ]; then
+        new_level=10
+    fi
+else
+    new_level=$(( current_level + step ))
+    if [ $new_level -ge 100 ]; then
+        new_level=100
+    fi
+fi
+
+/usr/bin/xbacklight -set $new_level
+
+/usr/bin/notify-send -u low -h string:x-canonical-private-synchronous:anything "Screen backlight [ $new_level% ]"
