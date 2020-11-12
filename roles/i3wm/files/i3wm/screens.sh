ecf8983330df30e479110a81c2573698d93b5fc2
Slawek Kaplonski <skaplons@redhat.com>
2020-11-12 17:34:20 +0100

    Change i3wm screen layout scripts
    Now I don't need scripts for 2 monitors anymore but I need
    script for 3 monitors.

--- roles/i3wm/files/i3wm/screens.sh
+++ roles/i3wm/files/i3wm/screens.sh
@@ -5,14 +5,14 @@ lid_status=$(cat /proc/acpi/button/lid/LID/state | grep state | awk -F':' '{prin
 
 if [ "$lid_status" == "open" ]; then
     # Now check if external displays are connected or not
-    xrandr | grep -v disconnected | grep -v eDP1 | grep connected -q
+    xrandr | grep -v disconnected | grep -v eDP-1 | grep connected -q
     if [ $? -eq 0 ]; then
-        /home/slaweq/.screenlayout/2_external_and_laptop.sh
+        /home/slaweq/.screenlayout/3_external_monitors.sh
     else
         /home/slaweq/.screenlayout/only_laptop.sh
     fi
 else
-    /home/slaweq/.screenlayout/2_external_closed_lid.sh
+    /home/slaweq/.screenlayout/3_external_monitors.sh
 fi
 
 # and also set correct Xserver settings
---
+++ roles/i3wm/files/screenlayouts/3_external_monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-3-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP-3-2 --mode 1920x1080 --pos 3000x254 --rotate normal --output DP-3-3 --primary --mode 1920x1080 --pos 1080x254 --rotate normal
