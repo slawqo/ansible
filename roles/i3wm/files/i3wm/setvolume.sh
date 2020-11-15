926b7a2ea8fa3bd0b40c6961d8f97ec2848dba2d
Slawek Kaplonski <skaplons@redhat.com>
2020-11-15 10:17:51 +0100

    Fix setvolume script for i3wm

--- roles/i3wm/files/i3wm/setvolume.sh
+++ roles/i3wm/files/i3wm/setvolume.sh
@@ -5,7 +5,7 @@ step=$2
 
 if [ "$action" == "mute" ]; then
     amixer -q sset Master,0 toggle
-    current_state=$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2" "$4 }')
+    current_state=$(amixer -D pulse sget Master | grep 'Mono:' | awk -F'[][]' '{ print $2" "$4 }')
     mute_state=$(echo $current_state | awk '{ print $2}')
     if [ "$mute_state" == "off" ]; then
         new_level=0%
@@ -18,7 +18,7 @@ else
     else
         amixer -q sset Master,0 $step%- unmute
     fi
-    new_level=$(amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }')
+    new_level=$(amixer sget Master | grep 'Mono:' | awk -F'[][]' '{ print $2 }')
 fi
 
 /usr/bin/notify-send -a "Volume" -u low -h string:x-canonical-private-synchronous:anything "[ $new_level ]"
