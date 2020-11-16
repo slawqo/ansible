9adc92922f70c5630d37b3ea39243a3d9a80dcf8
Slawek Kaplonski <skaplons@redhat.com>
2020-11-16 08:25:04 +0100

    Update i3wm monitors layout

--- roles/i3wm/files/screenlayouts/3_external_monitors.sh
+++ roles/i3wm/files/screenlayouts/3_external_monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-3-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP-3-2 --mode 1920x1080 --pos 3000x254 --rotate normal --output DP-3-3 --primary --mode 1920x1080 --pos 1080x254 --rotate normal
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP3-2 --mode 1920x1080 --pos 3000x254 --rotate normal --output DP3-3 --primary --mode 1920x1080 --pos 1080x254 --rotate normal
