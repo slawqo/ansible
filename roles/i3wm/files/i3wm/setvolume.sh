bd3f84db2669de74cc3717d5ff4ccd5b7e20eb67
Slawek Kaplonski <skaplons@redhat.com>
2020-11-21 19:54:16 +0100

    Make setvolume script to be compatible with pamixer
    It was using amixer earlier and it didn't work fine. With pamixer should
    be better.

--- roles/i3wm/files/i3wm/setvolume.sh
+++ roles/i3wm/files/i3wm/setvolume.sh
@@ -4,21 +4,14 @@ action=$1
 step=$2
 
 if [ "$action" == "mute" ]; then
-    amixer -q sset Master,0 toggle
-    current_state=$(amixer -D pulse sget Master | grep 'Mono:' | awk -F'[][]' '{ print $2" "$4 }')
-    mute_state=$(echo $current_state | awk '{ print $2}')
-    if [ "$mute_state" == "off" ]; then
-        new_level=0%
-    else
-        new_level=$(echo $current_state | awk '{print $1 }')
-    fi
+    pamixer --toggle-mute
 else
     if [ "$action" == "inc" ]; then
-        amixer -q sset Master,0 $step%+ unmute
+        pamixer --increase $step
     else
-        amixer -q sset Master,0 $step%- unmute
+        pamixer --decrease $step
     fi
-    new_level=$(amixer sget Master | grep 'Mono:' | awk -F'[][]' '{ print $2 }')
 fi
+new_level=$(pamixer --get-volume-human)
 
 /usr/bin/notify-send -a "Volume" -u low -h string:x-canonical-private-synchronous:anything "[ $new_level ]"
