263930636cd2428168ca602097e7ff9aa0b5cabc
Slawek Kaplonski <skaplons@redhat.com>
2020-11-22 23:47:34 +0100

    Update 3_external_monitors script
    It is kind of workaround for issue with enabling 3 monitors at once in
    xrandr

--- roles/i3wm/files/screenlayouts/3_external_monitors.sh
+++ roles/i3wm/files/screenlayouts/3_external_monitors.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP3-2 --mode 1920x1080 --pos 3000x379 --rotate normal --output DP3-3 --mode 1920x1080 --pos 1080x379 --rotate normal --output VIRTUAL1 --off
+xrandr --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate left
+xrandr --output DP3-3 --primary --mode 1920x1080 --pos 1080x326 --rotate normal
+xrandr --output DP3-2 --mode 1920x1080 --pos 3000x326 --rotate normal
+xrandr --output eDP1 --off
+xrandr --output DP1 --off
+xrandr --output DP2 --off
+xrandr --output DP3 --off
+xrandr --output VIRTUAL1 --off
