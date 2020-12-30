9e92a2b661fd5f10d846c1f6d7d2d93c53df6e23
Slawek Kaplonski <skaplons@redhat.com>
2020-12-30 23:44:33 +0100

    Add i3lock script
    It is using i3lock-color and shows blured desktop on the
    locked screen.

---
+++ roles/i3wm/files/i3wm/lock.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+BG='#282828'
+RED='#fb4934'
+GREEN='#b8bb26'
+GRAY='#444444'
+BLUE='#00aaff'
+PURPLE='#d3869b'
+YELLOW='#ffb700'
+FG='#f08379'
+
+
+#
+# Launch i3lock-color
+#
+i3lock \
+    --nofork \
+    --color='282828' \
+    --show-failed-attempts \
+    --ignore-empty-password \
+    --pass-media-keys \
+    --pass-screen-keys \
+    --pass-power-keys \
+    --blur 5 \
+    --radius 110 \
+    --ring-width 10 \
+    --clock \
+    --indicator \
+    --timestr="%H:%M:%S" \
+    --datestr="%d.%m.%Y" \
+    --timecolor="${FG}ff" \
+    --datecolor="${FG}ff" \
+    --linecolor="${BG}ff" \
+    --separatorcolor="${BG}ff" \
+    --insidecolor="${BG}dd" \
+    --insidewrongcolor="${BG}dd" \
+    --wrongcolor="${FG}ff"
+   #--verifcolor="${FG}ff" \
+   #--ringcolor="${GRAY}ee" \
+   #--keyhlcolor="${YELLOW}ff" \
+   #--bshlcolor="${PURPLE}ff" \
+   #--insidevercolor="${BG}dd"   \
+   #--ringvercolor="${BLUE}ee" \
+   #--ringwrongcolor="${RED}ee" \
