108fc4c1d81de2dffb55c98ac1d0f08e6ca32e5f
Slawek Kaplonski <skaplons@redhat.com>
2021-01-25 18:51:37 +0100

    Change 3 monitors layout

--- roles/i3wm/files/screenlayouts/3_external_monitors.sh
+++ roles/i3wm/files/screenlayouts/3_external_monitors.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xrandr --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate left
 xrandr --output DP3-3 --primary --mode 1920x1080 --pos 1080x326 --rotate normal
-xrandr --output DP3-2 --mode 1920x1080 --pos 3000x326 --rotate normal
+xrandr --output DP3-2 --mode 1920x1080 --pos 3000x0 --rotate right
 xrandr --output eDP1 --off
 xrandr --output DP1 --off
 xrandr --output DP2 --off
