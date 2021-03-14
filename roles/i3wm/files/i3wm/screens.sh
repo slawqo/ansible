8e7017953f73f0b403871183bb2ee0d116c09abf
Slawek Kaplonski <skaplons@redhat.com>
2021-03-14 16:15:12 +0100

    Add udev rule to configure displays automatically
    It also required some small improvements in the screens.sh script so
    additional logging and optional sleep was added.
    
    Sleep should be removed at some point when script will be more smart and
    will wait until all displays are really connected to configure them but
    for now such sleep is simple workaround and works pretty ok.

--- roles/i3wm/files/i3wm/screens.sh
+++ roles/i3wm/files/i3wm/screens.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+logger "Start display configuration script"
+sleep_time=$1
+
+if [ -z "$sleep_time" ]; then
+    sleep_time=0
+fi
+
+logger "Display configuration delayed by $sleep_time seconds"
+sleep $sleep_time
+
 # First check if LID is closed or not
 lid_status=$(cat /proc/acpi/button/lid/LID/state | grep state | awk -F':' '{print $2}' |tr -d " ")
 
@@ -7,11 +17,14 @@ if [ "$lid_status" == "open" ]; then
     # Now check if external displays are connected or not
     xrandr | grep -v disconnected | grep -v eDP | grep connected -q
     if [ $? -eq 0 ]; then
+        logger "LID opened, configuring 3 external monitors"
         /home/slaweq/.screenlayout/3_external_monitors.sh
     else
+        logger "LID opened, configuring only laptop display"
         /home/slaweq/.screenlayout/only_laptop.sh
     fi
 else
+    logger "LID closed, configuring 3 external monitors"
     /home/slaweq/.screenlayout/3_external_monitors.sh
 fi
 
@@ -21,3 +34,5 @@ xset b off
 
 # Set wallpaper
 /home/slaweq/.fehbg
+
+logger "Display configuration script finished"
