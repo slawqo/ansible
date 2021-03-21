28341807ef183803055816f7aea3aae4043d8898
Slawek Kaplonski <skaplons@redhat.com>
2021-03-21 11:18:20 +0100

    Change screens config script and run it after wakeing up laptop
    There is no need to check if lid is open or close in that script. If
    external displays are plugged in, they should be configured.

--- roles/i3wm/files/i3wm/screens.sh
+++ roles/i3wm/files/i3wm/screens.sh
@@ -10,22 +10,14 @@ fi
 logger "Display configuration delayed by $sleep_time seconds"
 sleep $sleep_time
 
-# First check if LID is closed or not
-lid_status=$(cat /proc/acpi/button/lid/LID/state | grep state | awk -F':' '{print $2}' |tr -d " ")
-
-if [ "$lid_status" == "open" ]; then
-    # Now check if external displays are connected or not
-    xrandr | grep -v disconnected | grep -v eDP | grep connected -q
-    if [ $? -eq 0 ]; then
-        logger "LID opened, configuring 3 external monitors"
-        /home/slaweq/.screenlayout/3_external_monitors.sh
-    else
-        logger "LID opened, configuring only laptop display"
-        /home/slaweq/.screenlayout/only_laptop.sh
-    fi
-else
-    logger "LID closed, configuring 3 external monitors"
+# Check if external displays are connected or not
+xrandr | grep -v disconnected | grep -v eDP | grep connected -q
+if [ $? -eq 0 ]; then
+    logger "Configuring 3 external monitors"
     /home/slaweq/.screenlayout/3_external_monitors.sh
+else
+    logger "Configuring only laptop display"
+    /home/slaweq/.screenlayout/only_laptop.sh
 fi
 
 # and also set correct Xserver settings
