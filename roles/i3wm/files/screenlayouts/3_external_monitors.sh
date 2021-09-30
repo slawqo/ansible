f1692b3267990d45f146e67ce3184ee420d27ee4
Slawek Kaplonski <skaplons@redhat.com>
2021-09-30 08:23:00 +0200

    [i3wm] Change screen layout to 2 vertical monitors and 1 horizontal

--- roles/i3wm/files/screenlayouts/3_external_monitors.sh
+++ roles/i3wm/files/screenlayouts/3_external_monitors.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-xrandr --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate normal
-xrandr --output DP3-2 --mode 1920x1080 --pos 3840x0 --rotate normal
-xrandr --output DP3-3 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
 xrandr --output eDP1 --off
 xrandr --output DP1 --off
 xrandr --output DP2 --off
 xrandr --output DP3 --off
+xrandr --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate left
+xrandr --output DP3-2 --mode 1920x1080 --pos 3000x0 --rotate right
+xrandr --output DP3-3 --primary --mode 1920x1080 --pos 1080x238 --rotate normal
 xrandr --output VIRTUAL1 --off
