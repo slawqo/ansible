3ac5a9770f872e505d930719246f1d40d2d9331c
Slawek Kaplonski <skaplons@redhat.com>
2021-09-30 16:07:37 +0200

    [i3wm] Adjust monitors setup

--- roles/i3wm/files/screenlayouts/3_external_monitors.sh
+++ roles/i3wm/files/screenlayouts/3_external_monitors.sh
@@ -5,5 +5,5 @@ xrandr --output DP2 --off
 xrandr --output DP3 --off
 xrandr --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate left
 xrandr --output DP3-2 --mode 1920x1080 --pos 3000x0 --rotate right
-xrandr --output DP3-3 --primary --mode 1920x1080 --pos 1080x238 --rotate normal
+xrandr --output DP3-3 --primary --mode 1920x1080 --pos 1080x377 --rotate normal
 xrandr --output VIRTUAL1 --off
